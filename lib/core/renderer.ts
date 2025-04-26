1cf65df5f75335bb9a64a6f01f90d94cb7aced92
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-26 21:39:29 +0530

    fix/error when importing component

--- lib/core/renderer.ts
+++ lib/core/renderer.ts
@@ -6,7 +6,7 @@ export default (content: string, options: Record<string, any>, filePath: string)
     const tokenizer: Lexer = new Lexer(content, filePath);
     const tokens = tokenizer.start();
     
-    const parserInstance: Parser = new Parser(tokens, options, filePath.replace(/\/[^\/]+$/, ''));
+    const parserInstance: Parser = new Parser(tokens, options, filePath.replace(/[\/\\][^\/\\]+$/, ''));
     const parsed: string = parserInstance.parse();
 
     const html = parserInstance.parameterExecuter(parsed)
