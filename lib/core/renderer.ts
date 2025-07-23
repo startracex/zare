7923901fe5e03150bb3a42354bc6e3a06fb22d92
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-07-23 19:39:51 +0530

    chore: rewrite the xss prevention logic

--- lib/core/renderer.ts
+++ lib/core/renderer.ts
@@ -1,12 +1,16 @@
 import Lexer from "./lexer.js";
 import Parser from "./parser.js";
+import { sanitizeOptions } from "../utils/helper.js";
 
 export default (content: string, options: Record<string, any>, filePath: string) => {
 
     const tokenizer: Lexer = new Lexer(content, filePath);
     const tokens = tokenizer.start();
-    
-    const parserInstance: Parser = new Parser(tokens, options, filePath.replace(/[\/\\][^\/\\]+$/, ''));
+
+    const sanitized = sanitizeOptions(options);
+    sanitized["_"] = options;
+
+    const parserInstance: Parser = new Parser(tokens, sanitized, filePath.replace(/[\/\\][^\/\\]+$/, ''));
     const parsed: string = parserInstance.parse();
 
     const html = parserInstance.parameterExecuter(parsed)
