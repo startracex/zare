5379aa5fe27e9164b855e6e70a1c565ea33b17e9
Shiro Wang <startraceebr@foxmail.com>
2025-07-24 15:09:12 +0800

    chore: add copy-licenses script

---
+++ scripts/copy-licenses.js
@@ -0,0 +1,9 @@
+import { getPackages } from "@manypkg/get-packages";
+import { copyFile } from "fs/promises";
+import { join, resolve } from "path";
+
+const root = resolve(import.meta.dirname, "..");
+
+const { packages } = await getPackages(root);
+
+await Promise.all(packages.map(({ dir }) => copyFile(join(root, "LICENSE"), join(dir, "LICENSE"))));
