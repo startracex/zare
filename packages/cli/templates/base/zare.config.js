d9274e47f6c12aeaed11affc862f172a6cc60498
Shiro Wang <startraceebr@foxmail.com>
2025-12-31 01:52:12 +0800

    feat: update template alias imports

--- packages/cli/templates/base/zare.config.js
+++ packages/cli/templates/base/zare.config.js
@@ -1,15 +1,11 @@
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export default {
   port: 8185,
   staticDir: './static',
   pagesDir: './pages',
   outDir: './dist',
-  alias: resolve(__dirname),
+  alias: {
+    '@/*': ['./*'],
+  },
   generateStaticParams(path) {
     switch (path) {
       case '/user/[id]':
